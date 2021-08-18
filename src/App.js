2d0345efd9d8db066fc3ade8f051a823a1f48910
raimondas-27 <raimondasvilnius@yahoo.com>
2021-08-18 12:05:22 +0300

    try/catch error case handling in process...

--- src/App.js
+++ src/App.js
@@ -4,39 +4,47 @@ import './App.css';
 import axios from "axios";
 
 function App() {
-   const dummyMovies = [
-      {
-         id: 1,
-         title: 'Some Dummy Movie',
-         openingText: 'This is the opening text of the movie',
-         releaseDate: '2021-05-18',
-      },
-      {
-         id: 2,
-         title: 'Some Dummy Movie 2',
-         openingText: 'This is the second opening text of the movie',
-         releaseDate: '2021-05-19',
-      },
-   ];
 
+   const [isLoading, setIsLoading] = useState(false);
    const [movies, setMovies] = useState([]);
+   const [movieError, setMovieError] = useState(false);
 
    const fetchMoviesHandler = async () => {
+      setIsLoading(true)
+      setMovieError(false);
       try {
-         const {data} = await axios.get("https://swapi.dev/api/films/");
-         setMovies(data.results)
+         const {data} = await axios.get("https://swapi.dev/api/film/");
+
+         //perdyti duomenis i mums reikalingus
+         console.log(data.results)
+         const moviesTransformed = data.results.map((oneMovie) => {
+            return {
+               id: oneMovie.episode_id,
+               title: oneMovie.title,
+               openingText: oneMovie.opening_crawl,
+               releaseDate: oneMovie.release_date,
+            };
+         })
+         setMovies(moviesTransformed);
       } catch (err) {
          console.log(err.message)
+         setMovieError(err.message);
       }
+      setIsLoading(false);
    }
 
    return (
        <React.Fragment>
           <section>
-             <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+             <button disabled={isLoading}
+                     onClick={fetchMoviesHandler}>
+                {isLoading ? "loading" : "Fetch Movies"}
+             </button>
           </section>
           <section>
-             <MoviesList movies={dummyMovies}/>
+             {!isLoading && movies.length > 0 && <MoviesList movies={movies}/>}
+             {!isLoading && movies.length === 0 && <p>No movies at the moment </p>}
+             {isLoading && <p>Loading ...</p>}
           </section>
        </React.Fragment>
    );
